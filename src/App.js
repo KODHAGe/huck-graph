b89ec33a49d594a34e23f9affbc21f352a72324c
Wolf Wikgren <wolf.wikgren@gmail.com>
2024-01-27 12:39:03 +0200

    remove icon with me own doings

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <div className="intro">
           <span id="title">
             <h1 className="title-heading">Huckleberry pie <span className="italic">(chart)</span></h1>
-            <img src="pie_1075891.png" className="title-icon"></img>
+            <img src="huckpie.svg" className="title-icon"></img>
           </span>
           <p>A tool that cooks some nice stats and graphs out of your Huckleberry data. 
             Works completely locally in your own browser, data is not stored or processed outside your own computer!</p>
@@ -33,7 +33,6 @@ function App() {
         </div>
       </div>
       <footer>
-        <a href="https://www.freepik.com/icon/pie_1075891#fromView=search&term=pie+slice+icon&track=ais&page=1&position=73&uuid=34c85a08-7f8d-41df-8dbd-92d952a6a5f3">Icon by Freepik</a>
       </footer>
     </div>
   );
